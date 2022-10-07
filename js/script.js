2ee136203ef7c7333f2a175258aabfb1ae1acc2e
Abdumalik <abdumalikcodes7753@gmail.com>
2022-10-07 19:11:00 +0500

    Your information

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// console.log(name + ' ' + surname + ' Your phone number:' + number);
+let name = prompt('What is your name?')
+let surname = prompt('What is your surname?')
+let age = prompt('How old are you?')
+let number = prompt('Enter your phone number!')
+
+console.log('Your name:' + name);
+
+console.log('Your surname:' + surname);
+
+console.log('Your age:'+ age);
+
+console.log('Your phone number:' + number);
